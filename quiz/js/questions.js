439db34df1de5ac0f7326de21efe67815842c016
MukulKumardav <88960732+MukulKumardav@users.noreply.github.com>
2021-08-15 12:27:35 +0530

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How is COVID-19 passed on?",
+    answer: "Through droplets that come from your mouth and nose when you cough or breathe out",
+    options: [
+      "Through droplets that come from your mouth and nose when you cough or breathe out",
+      "By drinking unclean water",
+      "none of the above ",
+      "all of the above "
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are the common symptoms of COVID-19? ",
+    answer: "All of the above",
+    options: [
+      "A new and continuous cough",
+      "Fever",
+      "Tiredness",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Can you always tell if someone has COVID-19? ",
+    answer: "No – not everyone with COVID-19 has symptoms",
+    options: [
+      "No – not everyone with COVID-19 has symptoms",
+      "Yes – it will be obvious, a person with COVID-19 coughs a lot",
+      "Yes – you can tell just by where a person comes from, their race and ethnicity",
+     
+    ]
+  },
+    {
+    numb: 4,
+    question: "Can washing your hands protect you from COVID-19? ",
+    answer: "Yes – normal soap and water or hand sanitizer is enough",
+    options: [
+      "Yes – but only if you use a strong bleach",
+      "Yes – normal soap and water or hand sanitizer is enough",
+      
+      "No – Washing your hands doesn’t stop COVID-19"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Are people living with HIV always more at risk? ",
+    answer: "No – people who adhere to antiretroviral treatment (ART) and have a high CD4 count aren’t more at risk",
+    options: [
+      "Yes – people with HIV have weaker immune systems",
+      "No – people who adhere to antiretroviral treatment (ART) and have a high CD4 count aren’t more at risk",
+     
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
